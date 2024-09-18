661cdc6cf5487ffaef5984c5371270dce9b86410
Daniel Boj Cobos <dboj@uoc.edu>
2024-09-18 07:19:45 +0000

    Gunicorn ready

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ app = create_app()
 
 if __name__ == '__main__':
 
-    # from gunicorn.app.wsgiapp import WSGIApplication as gunicorn
-    # app = gunicorn(app)
-    app.run(host= '0.0.0.0', port='5000')
+    from gunicorn.app.wsgiapp import WSGIApplication as gunicorn
+    app = gunicorn(app)
+    # app.run(host= '0.0.0.0', port='5000')
